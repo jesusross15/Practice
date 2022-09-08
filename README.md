d5bd6fce256d1cc8ebc35efa158826b3fe9b2ed5
Jesus <jesusross15@gmail.com>
2022-09-08 18:35:53 -0400

    Stack implementation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Practice
 This repository is for all my practicing code needs.
+The main reason for this is just to make sure I am practicing how to code every single day. Every day until I am officialy a software engineer. 
---
+++ practice1.py
@@ -0,0 +1,16 @@
+# Implementation of a stack using a list
+
+stack = ["Jesus", "Zander", "Aaron"]
+
+# Insertion of values to a stack
+stack.append("Brandon")
+stack.append("John")
+print(stack)
+
+# Deletion of last value of stack 
+print(stack.pop())
+
+# New stack after deletion of stack has been done
+print(stack)
+
+
