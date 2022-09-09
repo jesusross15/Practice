9abdc47423cdf0a2f468e2cdd1bc40b3b6a5a0ae
Jesus <jesusross15@gmail.com>
2022-09-09 19:50:26 -0400

    cleak code

--- practice2.py
+++ practice2.py
@@ -1,7 +1,8 @@
 # Python program to interchange first and last elements in a list
 """Given a list, write a Python prgoram to swap first and last element of the list."""
-print("1st approach")
+print("First approach:")
 # Approach 1: find the length of the list and swap the firt element w the (n-1)th (last) element
+list = [1, 42, 2, 9, 15, 17, 21]
 # function for swap
 def swap(list):
     size = len(list)
@@ -14,14 +15,13 @@ def swap(list):
     return list
 
 # testing list
-list = [1, 42, 2, 9, 15, 17, 21]
+print("This is the original list: " + str([1, 42, 2, 9, 15, 17, 21]))
+print("This is the swapped list: " + str(swap(list)))
 
-print(swap(list))
-
-print("\nSecond approach")
+print("\nSecond approach:")
 #approach #2
 # if we refer to the last element of the list as list[-1] we can simply swap index [0] w index [-1]
-
+new_list = [19, 42, 16, 18, 22, 89]
 # swap function
 def swap_list(new_list):
     
@@ -29,7 +29,7 @@ def swap_list(new_list):
     return new_list
 
 #testing code
-new_list = [19, 42, 16, 18, 22, 89]
-print(swap_list(new_list))
+print("This is the original list: " + str([19, 42, 16, 18, 22, 89]))
+print("This is the swapped list: " + str(swap_list(new_list)))
 
     
