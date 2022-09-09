ddd09efecad7cc5bbd6eb5309899ae75ceaba060
Jesus <jesusross15@gmail.com>
2022-09-08 20:48:42 -0400

    testing different colors

--- clock.py
+++ clock.py
@@ -11,7 +11,7 @@ def time():
     label.config(text=string)
     label.after(1000, time)
     
-label = Label(root, font=("Times", 80), background = "black", foreground = "black")
+label = Label(root, font=("Times", 80), background= "red", foreground = "black")
 label.pack(anchor="center")
 
 time()
