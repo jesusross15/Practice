dc2a4238df688dc30379da8c3ad66bdb84ad5a20
Jesus <jesusross15@gmail.com>
2022-09-10 23:11:34 -0400

    practicing functions

---
+++ practice3.py
@@ -0,0 +1,19 @@
+"""Write a program that takes a list and returns a new list 
+that contains all the elements of the first list minus all the duplicates."""
+
+# let's solve this using a function first a for loop
+def dupe1(x):
+    y = []
+    for i in x:
+        if i not in y:
+            y.append(i)
+    return y
+
+#this one is a function using sets
+def dupe2(x):
+    return list(set(x))
+
+a = [1,2,2,2,4,5,5,6,7,8,8,8,8,8,9]
+print(a)
+
+print(dupe1(a))
